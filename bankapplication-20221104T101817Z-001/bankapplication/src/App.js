9a3edf7e12c1fe4c6d0c39a68438f593c2deaef9
Sparsh Jain <jainsparsh0801@gmail.com>
2022-11-07 12:44:25 +0530

    user story 4: apply loan

--- bankapplication-20221104T101817Z-001/bankapplication/src/App.js
+++ bankapplication-20221104T101817Z-001/bankapplication/src/App.js
@@ -4,10 +4,12 @@ import Userlogin from "./Components/Userlogin";
 import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
 import UserRegisteration from "./Components/UserRegisteration";
 import UserMenu from "./Components/UserMenu";
+import ApplyLoan from "./Components/ApplyLoan";
+import TransactionManagement from "./Components/TransactionManagement";
 function App() {
     return (
         <BrowserRouter>
-            <main className='d-flex flex-column'>
+            <main className='main d-flex flex-column'>
                 <Routes>
                     <Route path='/' element={<UserMenu />} />
                 </Routes>
@@ -16,6 +18,15 @@ function App() {
                 </Routes>
                 <Routes>
                     <Route path='/register' element={<UserRegisteration />} />
+                </Routes>
+                <Routes>
+                    <Route path='/applyLoan' element={<ApplyLoan />} />
+                </Routes>
+                <Routes>
+                    <Route
+                        path='/transactions'
+                        element={<TransactionManagement />}
+                    />
                 </Routes>
             </main>
         </BrowserRouter>
---
+++ bankapplication-20221104T101817Z-001/bankapplication/src/Components/ApplyLoan.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+    Button,
+    Form,
+    FormGroup,
+    Input,
+    FormFeedback,
+    Label,
+} from "reactstrap";
+const validateAmount = (val) => !isNaN(val) && val >= 0 && val <= 100000;
+const validateField = (field, value) => {
+    switch (field) {
+        case "amount":
+            return validateAmount(value);
+        case "branch":
+            return true;
+        default:
+            return false;
+    }
+};
+const branches = ["main", "temp"];
+const ApplyLoan = () => {
+    const [loan, setLoan] = useState({
+        userId: "12345678",
+        branch: branches[0],
+    });
+    const [valid, setValid] = useState({
+        amount: true,
+    });
+    const apply = () => {};
+    const handleChange = async (e) => {
+        setLoan({
+            ...loan,
+            [e.target.name]: e.target.value,
+        });
+        await setValid({
+            ...valid,
+            [e.target.name]: validateField(e.target.name, e.target.value),
+        });
+    };
+    return (
+        <div className='auth d-flex flex-row justify-content-center'>
+            <div
+                className='ml-auto mr-auto col-12 col-md-7 col-lg-6 form '
+                style={{ marginTop: "100px" }}>
+                <h2>Global Bank User loan application Page</h2>
+                <Form onSubmit={apply}>
+                    <FormGroup>
+                        <Label for='userId'>Customer ID</Label>
+                        <Input
+                            type='text'
+                            name='userId'
+                            id='userId'
+                            placeholder='userId'
+                            value={loan.userId}
+                            // onChange={handleChange}
+                            required
+                            disabled
+                            // invalid={
+                            //     loan?.userId === "" || !valid.userId
+                            // }
+                        />
+                        <FormFeedback>
+                            Please enter a 8 character valid UserId
+                        </FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='amount'>Select the branch</Label>
+                        <Input
+                            type='select'
+                            name='branch'
+                            id='branch'
+                            onChange={handleChange}>
+                            {branches.map((branch) => (
+                                <option key={branch}>{branch}</option>
+                            ))}
+                        </Input>
+                        {/* <Input
+                            type='amount'
+                            name='amount'
+                            id='amount'
+                            placeholder='Loan Amount'
+                            onChange={handleChange}
+                            required
+                            invalid={loan?.amount === "" || !valid.amount}
+                        /> */}
+                        <FormFeedback>
+                            Amount should be between 0 and 100000
+                        </FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='amount'>Enter the Loan Amount</Label>
+
+                        <Input
+                            type='amount'
+                            name='amount'
+                            id='amount'
+                            placeholder='Loan Amount'
+                            onChange={handleChange}
+                            required
+                            invalid={loan?.amount === "" || !valid.amount}
+                        />
+                        <FormFeedback>
+                            Amount should be between 0 and 100000
+                        </FormFeedback>
+                    </FormGroup>
+
+                    <FormGroup className=''>
+                        <>
+                            <div className=' text-align-end ml-auto mr-auto'>
+                                <div
+                                    className='ml-auto mr-auto'
+                                    style={{ width: "fit-content" }}>
+                                    <Button
+                                        color='primary'
+                                        className='ml-auto mr-auto'
+                                        disabled={!valid.amount}>
+                                        Apply
+                                    </Button>
+                                </div>
+                            </div>
+                        </>
+                    </FormGroup>
+
+                    {/* <FormGroup style={{ marginTop: "50px" }}>
+                        <h5 className='text-align-center'>
+                            Welcome to the bank,{" "}
+                            <Link to='/register'>Click here to register</Link>
+                        </h5>
+                    </FormGroup> */}
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default ApplyLoan;
